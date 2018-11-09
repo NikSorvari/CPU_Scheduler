bb4089128e628715c8322f749a05d8c1748a7727
Nik Sorvari <nikolassorvari@csus.edu>
2018-11-08 17:05:59 -0800

    update

--- src/Process.java
+++ src/Process.java
@@ -1,6 +1,14 @@
 
 public class Process {
 
+	public static boolean isDone;
+	public static String processNum;
+	public static int arrivalTime;
+	public static boolean arrived;
+	public static int remainingBurstTime;
+	public static int priorityNum;
+	public static int waitTime;
+
 	public Process(int num, int num1, int num2, int num3) {
 		// TODO Auto-generated constructor stub
 	}
--- src/Scheduling.java
+++ src/Scheduling.java
@@ -55,7 +55,7 @@ public class Scheduling {
 	public static void CheckProcessFinished()
 	{
 		for(int i = 0; i<fileRead.processList.size(); i++) {
-			if(fileRead.processList.get(i).hasFinished == false) {
+			if(fileRead.processList.get(i).isDone == false) {
 				doneProcesses = 0;
 			}
 			else {
@@ -65,6 +65,198 @@ public class Scheduling {
 			if(doneProcesses == fileRead.processNum) {
 				allProcessesDone = true;
 			}
+		}
+	}
+	
+	public static void CheckProcessArrived()
+	{
+		for(int i = 0; i<fileRead.processList.size(); i++) {
+			if(fileRead.processList.get(i).arrivalTime == t) {
+				fileRead.processList.get(i).arrived = true;
+				if(!(readyQ.contains(fileRead.processList.get(i)))) 
+					readyQ.add(fileRead.processList.get(i));
+			}
+		}
+	}
+	
+	public void checkReadyQEmpty() {
+		if(readyQ.isEmpty()) {
+			this.processInReadyQ = false;
+		}
+		else {
+			this.processInReadyQ = true;
+		}
+	}
+	
+	public static void checkRR() {
+		System.out.println("Now checking roundrobin");
+			if(runningQ.isEmpty()) {
+				runningQ.add(readyQ.get(0));
+				readyQ.remove(0);
+				timeLeft = defTimeQuantum;
+				outputArray.add(""+t + " "+ runningQ.get(0).processNum);
+				
+			}
+			if(!(runningQ.isEmpty()) && timeLeft == 0 ) {
+				readyQ.add(runningQ.get(0));
+				runningQ.clear();
+				if(runningQ.isEmpty()) {
+					runningQ.add(readyQ.get(0));
+					readyQ.remove(0);
+					timeLeft = defTimeQuantum;
+					outputArray.add(""+t + " "+ runningQ.get(0).processNum);
+					
+				}
+			}
+		
+	}
+	
+	public static void checkSJF() {
+		int shortestCPUTime = Integer.MAX_VALUE;
+		//adds the process with the highest priority into the running queue from ready queue
+		if(runningQ.isEmpty()) {					
+			for(int i = 0; i<readyQ.size(); i++) {
+				if(readyQ.get(i).remainingBurstTime < shortestCPUTime) {
+					shortestCPUTime = readyQ.get(i).remainingBurstTime;
+				}
+			}
+			for(int i = 0; i<readyQ.size(); i++) {
+				if(readyQ.get(i).remainingBurstTime == shortestCPUTime) {
+					runningQ.add(readyQ.get(i));
+					outputArray.add(""+t + " "+ readyQ.get(i).processNum);
+					
+					readyQ.remove(i);
+				}
+			}
+			
+		}
+	}
+	
+	public static void checkPWP() {
+		int highestPriorityInReady = Integer.MAX_VALUE;
+		int priorityInRun = Integer.MAX_VALUE;
+		
+		if(runningQ.isEmpty()) {					
+			for(int i = 0; i<readyQ.size(); i++) {
+				if(readyQ.get(i).priorityNum < highestPriorityInReady) {
+					highestPriorityInReady = readyQ.get(i).priorityNum;
+				}
+			}
+			for(int i = 0; i<readyQ.size(); i++) {
+				if(readyQ.get(i).priorityNum == highestPriorityInReady) {
+					runningQ.add(readyQ.get(i));
+					outputArray.add(""+t + " "+ readyQ.get(i).processNum);
+					
+					readyQ.remove(i);
+				}
+			}
+			
+		}
+		else {
+			System.out.println("there is a process in the running queue");
+			priorityInRun = runningQ.get(0).priorityNum;
+			
+			for(int i = 0; i<readyQ.size(); i++) {
+				if(readyQ.get(i).priorityNum < highestPriorityInReady) {
+					highestPriorityInReady = readyQ.get(i).priorityNum;
+				}
+			}
+			if(highestPriorityInReady < priorityInRun) {
+				tempQ.addAll(runningQ);
+				runningQ.clear();
+				for(int i = 0; i< readyQ.size(); i++) {
+					if(readyQ.get(i).priorityNum == highestPriorityInReady) {
+						readyQ.add(tempQ.get(0));
+						runningQ.add(readyQ.get(i));
+						outputArray.add(""+t + " "+ readyQ.get(i).processNum);
+						readyQ.remove(i);
+						tempQ.clear();
+					}
+				}
+			}
+		}
+		
+	}
+	
+	public static void checkPNP() {
+		int highestPriorityInReady = Integer.MAX_VALUE;
+		//adds the process with the highest priority into the running queue from ready queue
+		if(runningQ.isEmpty()) {					
+			for(int i = 0; i<readyQ.size(); i++) {
+				if(readyQ.get(i).priorityNum < highestPriorityInReady) {
+					highestPriorityInReady = readyQ.get(i).priorityNum;
+				}
+			}
+			for(int i = 0; i<readyQ.size(); i++) {
+				if(readyQ.get(i).priorityNum == highestPriorityInReady) {
+					runningQ.add(readyQ.get(i));
+			//		outputArray.add("Time of Preemption: run is empty " + Time + " of Process: " + readyProcessQueue.get(i).processNum);
+					outputArray.add(""+t + " "+ readyQ.get(i).processNum);
+					
+					readyQ.remove(i);
+				}
+			}
+			
+		}
+		
+	}
+	
+	public static void subtractRemainingTime() {
+		for(int i = 0; i<runningQ.size(); i++) {
+			runningQ.get(i).remainingBurstTime --;
+			System.out.println("Now subtracting cpu time from Process:" + runningQ.get(i).processNum);
+		
+			if(runningQ.get(i).remainingBurstTime <= 0) {
+				runningQ.get(i).isDone = true;
+				System.out.println("Process # " + runningQ.get(i).processNum + " has finished!!!");
+				runningQ.clear();
+				
+			}
+		}
+	}
+	
+	public static void addWaitTimeReadyQ() {
+		for(int i = 0; i<readyQ.size();i++) {
+			readyQ.get(i).waitTime ++;
+		}
+	}
+	
+	public static  double calculateAvgWaitTime() {
+		double avgTime = 0.0;
+		for(int i = 0; i<fileRead.processList.size();i++) {
+			avgTime = avgTime + fileRead.processList.get(i).waitTime;
+		}
+		avgTime = avgTime/fileRead.numberOfProcesses;
+		return avgTime;
+	}
+	
+	//prints out all of the processes info. the information is from an arraylist that is stored in the ReadFile class
+	public static void printOutAllProcessInfo() {
+		for(int i =0; i<r.processList.size();i++) {
+			System.out.println(r.processList.get(i).toSring());
+		}
+	}
+	
+	//prints out what would be written to the output.txt. This is just used as a check to see if things that will be 
+	//written to the output.txt will be written correctly.
+	public static void printOutOutputArray() {
+		for(int i = 0; i<outputArray.size(); i++) {
+			System.out.println(outputArray.get(i).toString());
+		}
+	}
+	
+	
+	//not used but if it did it will just check to see if the number of total processes match the number on line 2 of input.txt
+	public static void inputNumOfProcessWarning() {
+		int numOfProcess2ndLine = r.numberOfProcesses;
+		int actualNumOfProcess = r.processList.size();
+		if(numOfProcess2ndLine != actualNumOfProcess) {
+			System.out.println("WARNING CHECK YOUR INPUT. THE NUMBER OF PROCESSES YOU PUT ON THE SECOND LINE DOES NOT EQUAL......");
+			System.out.println("....THE AMOUNT OF TOTAL PROCESSES THAT ARE READ IN THE INPUT. YOU WILL GET AN INCORRECT OUTPUT!!!!");
+			bolOfProcessCheckCorrect = false;
+		}
+		else {
+		bolOfProcessCheckCorrect = true;
 		}
 	}
 
